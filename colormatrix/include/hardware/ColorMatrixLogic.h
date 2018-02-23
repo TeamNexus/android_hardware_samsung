0dfa60605dd0cd570ae2136aa828c73fe22893bb
Lukas0610 <mail@lukasberger.at>
2018-02-23 16:14:44 +0100

    colormatrix: add implementation for custom color-matrix logic
    Follows https://github.com/TeamNexus/android_frameworks_native/commit/614ea12143c0eb86b85056ced03d6d1cccec7c85
    
    This adds an usabel implementation of a custom logic for applying color-matricies
    for Samsung-devices (maybe even others) with MDNIE-support.
    
    (At least) on Android 8.1 and newer this will
    keep the H/W-composer enabled even if the system requested to set a color-matrix,
    which isn't the case without a custom logic
    
    Change-Id: I11d28ec2989187469308c72aa506583fac346812

---
+++ colormatrix/include/hardware/ColorMatrixLogic.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2018 TeamNexus
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * This file contains the dynamic/device-dependent configurations for the
+ * custom color-matrix logic
+ */
+
+#ifndef HARDWARE_COLORMATRIXLOGIC_H_INCLUDED
+#define HARDWARE_COLORMATRIXLOGIC_H_INCLUDED
+
+#define TARGET_COLOR_MATRIX_SENSOR_RGB   "/sys/class/mdnie/mdnie/sensorRGB"
+
+#endif // HARDWARE_COLOR_MATRIX_LOGIC_H_INCLUDED
