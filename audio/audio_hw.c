60b7a93fd5399a43e2496e15c10e6afa0bc2cf05
Lukas0610 <mail@lukasberger.at>
2018-06-18 18:02:55 +0200

    audio: disable VV-debugging
    Change-Id: I657461c18aac90dc4590817b039b789e00c89d63

--- audio/audio_hw.c
+++ audio/audio_hw.c
@@ -19,7 +19,7 @@
 
 #define LOG_TAG "audio_hw_primary"
 #define LOG_NDEBUG 0
-#define VERY_VERY_VERBOSE_LOGGING
+// #define VERY_VERY_VERBOSE_LOGGING
 #ifdef VERY_VERY_VERBOSE_LOGGING
 #define ALOGVV ALOGV
 #else
